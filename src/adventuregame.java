35a9b95e73e60ba058e7f7169aac8204d7148e74
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-08 21:19:09 -0400

    step5

--- src/adventuregame.java
+++ src/adventuregame.java
@@ -11,20 +11,31 @@ public class adventuregame {
 		
 		//Enter game answer into variable
 		Scanner sc1 = new Scanner(System.in);
-		System.out.println("Would you like to dive into a deep cave or swim with dolphins?(enter 1 for cave or 2 for dolphins)");
+		System.out.println("Would you like to sit on beach or swim with dolphins?(enter 1 for beach or 2 for dolphins)");
 		String enteredGameAnswer = sc1.nextLine();
 		
 		switch (enteredGameAnswer){
 		
 		case "1":
-		System.out.println("You dive into a deep cave and you spot a large neon orange fish with large teeth. Enter 1 to catch or 2"
-				+ "to take a photo");	
+		System.out.println("You decide to sit on the beach, but after an hour a unexpected tidal wave comes and kills you");	
 		break;
 
 		case "2":
-		System.out.println("You swim towards the dolphins only to realize there are also a group of sharks. Do you "
+		System.out.println("You swim towards the dolphins only to realize there are also a group of sharks."
 				+ "What would you do??(enter \"face the sharks\" or \"swim away\")");
-String faceOrSwim = sc1.nextLine();
-	}
+		String faceOrSwim = sc1.nextLine();
+	
+		switch (faceOrSwim){
+		
+		case "swim away":
+		System.out.println("You are swimming back quickly when a shark comes out of nowhere and bites you in half.");	
+		break;
+		
+		case "face the sharks":
+		System.out.println("You swim towards the sharks. You see there are __ number of sharks (enter\"1\" or \"2\" or \"3\"):");
+		int Number = sc1.nextInt();				
+		System.out.println("You have never seen " + Number + "sharks this close before! You decide to fight.");
+		System.out.println("Choose your weapon!(enter \"speargun\" or \"fist\" or \"bomb\")");
+}
 
 }
