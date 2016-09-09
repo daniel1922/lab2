9e989fb6f9dc98505fb35f4ea2c116fccc496a15
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-08 21:00:52 -0400

    step4

--- src/adventuregame.java
+++ src/adventuregame.java
@@ -13,6 +13,18 @@ public class adventuregame {
 		Scanner sc1 = new Scanner(System.in);
 		System.out.println("Would you like to dive into a deep cave or swim with dolphins?(enter 1 for cave or 2 for dolphins)");
 		String enteredGameAnswer = sc1.nextLine();
+		
+		switch (enteredGameAnswer){
+		
+		case "1":
+		System.out.println("You dive into a deep cave and you spot a large neon orange fish with large teeth. Enter 1 to catch or 2"
+				+ "to take a photo");	
+		break;
+
+		case "2":
+		System.out.println("You swim towards the dolphins only to realize there are also a group of sharks. Do you "
+				+ "What would you do??(enter \"face the sharks\" or \"swim away\")");
+String faceOrSwim = sc1.nextLine();
 	}
 
 }
