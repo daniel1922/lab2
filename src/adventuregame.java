926e56daf7cff0c635be22382ab6bd350ec20196
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-08 20:26:32 -0400

    step1

---
+++ src/adventuregame.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class adventuregame {
+
+	public static void main(String[] args) {
+		
+		//Enter name into variable
+		Scanner sc = new Scanner(System.in);	
+		System.out.println("Welcome! What is your name?(enter your name)");
+		String enteredName = sc.nextLine();
+	}
+
+}
